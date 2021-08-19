21e63725ee0d1bb692abc5d0f24ca0a155aa292f
newkayak12 <newkayak12@gmail.com>
2021-08-19 18:17:56 +0900

    react Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className="numbering"> 01. reactStart</h1>
+        <h1 className="title">React Start!</h1>
+        <h2> learn react</h2>
+
+        <br></br>
+        <hr></hr>
+        <h1 className="numbering">02 reactSample</h1>
+        <div>
+          <img className="App-logo " src="/logo.png" />
+          <p style={{fontSize:50}}> Hello! </p>
+        </div>
       </div>
     );
   }
