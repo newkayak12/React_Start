2de66051da6f94a0044ed9c0ec34ce29c94caf55
newkayak12 <newkayak12@gmail.com>
2021-10-29 22:50:10 +0900

    내부 슬롯

--- src/00javascript/05property/internalSlotAndMethod.js
+++ src/00javascript/05property/internalSlotAndMethod.js
@@ -33,3 +33,15 @@
 	// property attribut 정보를 제공하는 프로퍼티 디스크립터 객체를 반환한다. 
 	console.log(Object.getOwnPropertyDescriptor(person,'name'));
 	// { value: 'LEE', writable: true, enumerable: true, configurable: true }
+
+	person.age = 20;
+
+	console.log(Object.getOwnPropertyDescriptors(person));
+	// ES8에 모든 프로퍼티의 프로퍼티 어트리뷰트 정보를 제공하는 프로퍼티 디스크립터 객체를 반환한다. 
+	/*
+		{
+			name: {value: 'LEE', writable: true, enumerable: true, configurable: true},
+			age: { value: 20, writable: true, enumerable: true, configurable: true }
+		}
+
+	*/
