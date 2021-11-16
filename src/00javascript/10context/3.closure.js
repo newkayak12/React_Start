18b60ae607fe915589f2e21344c46f1157b812b6
newkayak12 <newkayak12@gmail.com>
2021-11-16 22:50:16 +0900

    closure

---
+++ src/00javascript/10context/3.closure.js
@@ -0,0 +1,9 @@
+// 클로저
+/**
+ * 클로저는 JS의 고유 개념이 아니다. 함수를 일급 객체로 취급하는 함수형 프로그래밍 언어(Haskell, Lisp, Erlang, Scala)에서 사용되는 중요한 특성이다.
+ * 클로저는 JS의 고유 개념이 아니므로 클로저의 정의가 ECMAScript 사양에 등장하지 않는다.
+ * 
+ * MDN에서 정의는 아래와 같다.
+ * 	"A closure is the combination of a function and the lexical environment within which that fucntion was declared."
+*			 		클로저는 함수와 그 함수가 선언된 렉시컬 환경의 조합이다. 
+ */
