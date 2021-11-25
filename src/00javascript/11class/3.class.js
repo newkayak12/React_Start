4e758987edbe6851c8b34012cbfe0cccc7a207e5
newkayak12 <newkayak12@gmail.com>
2021-11-25 22:16:12 +0900

    class

--- src/00javascript/11class/3.class.js
+++ src/00javascript/11class/3.class.js
@@ -247,3 +247,156 @@ class Person{
 
 	console.log(MyMath.PI)
 	console.log(MyMath.increment())
+
+
+
+// 상속
+// 상속에 의한 클래스 확장은 프로토타입 기반 상속과는 다른 개념이다. 
+// 프로토타입 기반 상속은 프로토타입 체인을 통해 다른 객체의 자산을 상속받는 개념이지만
+// 상속에 의한 클래스 확장은 기존 클래스를 상속받아 새로운 클래스를 확장하여 정의하는 것이다. 
+
+/**
+ * 클래스와 생성자 함수는 인스턴스를 생성할 수 있는 함수라는 점에서 매우 유사하다. 
+ * 하지만 클래스는 상속을 통해 기존 클래스를 확장할 수 있는 문법이 기본적으로 제공되지만 생성자 함수는 그렇지 않다.
+ * 
+ */
+
+class Animal{
+	constructor(age, weight) {
+		this.age = age;
+		this.weight = weight;
+	}
+
+	eat(){ return "eat" }
+	move(){ return "move" }
+}
+
+class Bird extends Animal{
+	fly(){ return "fly" }
+}
+
+const bird = new Bird(1,5)
+console.log(bird)
+console.log(bird instanceof Bird) //true
+console.log(bird instanceof Animal);//true
+
+console.log(bird.eat())
+console.log(bird.move())
+console.log(bird.fly())
+
+// 생성자 함수는 클래스와 같이 상속을 통해 다른 생성자 함수를 확장할 수 있는 문법이 제공되지 않는다. 
+
+
+
+//extends
+/**
+ * 수퍼클래스와 서브클래스는 인스턴스 프로토타입 체인뿐 아니라 클래스 간의 프로토타입 체인도 생성한다. 
+ * 이를 통해 프로토타입 메소드, 정적 메소드 모두 상속이 가능하다. 
+ */
+
+
+//동적상속 > extends 키워드는 클래스뿐만 아니라 생성자 함수를 상속받아 클래스를 확장할 수도 있다. 
+// extneds 키워드 앞에는 반드시 클래스가 와야한디.
+
+//  생성자 함수
+function Base(a){
+	this.a = a; 
+}
+
+//생성자 함수를 상속받는 서브클래스
+class Derived extends Base{}
+
+
+const derived = new Derived(1);
+console.log(derived); // Derived {a:1}
+
+/**
+ * extends 키워드는 다음에는 클래스뿐만 아니라 [[Construct]] 내부 메소드를 갖는 함수 객체로 평가될 수 있는
+ * 모든 표현식을 사용할 수 있다. 이를 통해 동적으로 상속받을 대상을 결정할 수 있다. 
+ */
+
+function Base1(){}
+class Base2{}
+
+let condition = true;
+
+//조건에 따라 동적으로 상속 대상을 결정하는 서블클래스
+class Derived extends (condition? Base1:Base2){}
+
+
+
+
+
+//>>서브클래스의 constructor
+//constructor를 생략하면 클래스에 비어있는 constructor가 암묵적으로 정의된다.
+//super()은 수퍼 클래스의 생성자를 호출한다. : 해당 키워드는 함수처럼 호출할 수도 있고 this와 같이 식별자처럼 참조할 수 있는 특수한 키워드이다.
+//super는 아래와 같이 동작한다. 
+/**
+ *  1. super를 호출하면 수퍼클래스의 constructor(super-constructor)를 호출한다.
+ *  2. super를 참조하면 수퍼클래스의 메소드를 호출할 수 있다. 
+ */
+//super를 호출하면 수퍼 클래스의 constructor(super-constructor)를 호출한다. 
+
+
+//super를 호출할 떄 주의 사항
+/**
+ * 1. 서브클래스에서 constructor를 생략하지 않는 서브클래스의 constructor에서는 반드시 super를 호출해야 한다. 
+ * 2. 서브클래스의  constructor에서 super를 호출하기 전에는 this를 참조할 수 없다. 
+ * 3. super는 반드시 서브클래스의 constructor에서만 호출한다. 서브클래스가 아닌 클래스의 constructor나 함수에 super를 호출하면 에러가 발생한다. 
+ */
+
+
+
+
+//super 참조 : 메소드 내에서 super를 참조하면 수퍼클래스의 메소드를 호출할 수 있다.
+/**
+ * 1. 서브클래스의 프로토타입 메소드 내에서 super.sayHi는 수퍼클래스의 프로토타입 메소드 sayHi를 가리킨다. 
+ * 	(super참조를 통해 수퍼 클래스의 메소드를 참조하려면 super가 수퍼클래스의 메소드가 바인딩된 객체, 
+ * 	즉 수퍼클래스의 prototype프로퍼티는에 바인딩된 프로토타입을 참조할 수 있어야 한다.)
+ */
+
+class SuperClass{
+	constructor(name){
+		this.name = name;
+	}
+
+	sayHi(){
+		return `Hi! ${this.name}`;
+	}
+}
+
+class SubClass extends SuperClass{
+	sayHi(){
+		// return `${super.sayHi()}. how are you doing?`
+
+		const __super = Object.getPrototypeOf(SubClass.prototype); //__super는 Base.prototype을 가리킨다. 
+		return `${__super.sayHi.call(this)} how are you doing`
+	}
+
+}
+/**
+ * super 참조를 통해 수퍼클래스의 메소드를 참조하려면 super가 수퍼클래스의 메소드가 바인딩된 객체 즉, 수퍼클래스의 prototype 프로퍼티에 바인딩된 프로토타입을 참조할 수 있어야한다. 
+ * super는 자신을 참조하고 있는 메소드가 바인딩되어 있는 객체의 프로토타입을 가리킨다. 따라서 super.sayHi는 Base.prototype.sayHi를 가리킨다. 
+ * 즉 Base.prototype.sayHi를 호출할 때 call 메소드를 사용해 this를 전달해야 한다. 
+ * call메소드를 사용해 this를 전달하지 않고 Base.prototype.sayHi를 그대로 호출하면 Base.prototype.sayHi메소드 내부의 this는  Base.prototype을 가리킨다. 
+ * 
+ * 
+ * 이와 같이 super 참조가 동작하기 위해서는 super를 참조하고 있는 메소드가 바인딩되어 있는 객체의 프로토 타입을 찾을 수 있어야한다.
+ * 이를 위해 메소드는 내부 슬롯 [[HomeObject]]를 가지며, 자신을 바인딩하고 있는 객체를 가진다.  ([[HomeObject]]를 가지는 함수만이 super참조를 할 수 있다.)
+ */
+
+const superDecl = {
+	name:'lee',
+	sayHi(){
+		return `Hi! ${this.name}`
+	}
+}
+
+const subDecl = {
+	__proto___:superDecl,
+	sayHi(){
+		return `${super.sayHi()}. how are you doing?`
+	}
+}
+
+//2 서브클래스의 정적 메소드 내에서 super.sayHi는 수퍼클래스의 정적 메소드 sayHi를 가진다. 
