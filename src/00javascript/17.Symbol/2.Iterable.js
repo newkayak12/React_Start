541863d3d591479b6c58ce25bb98e0110a9ca256
newkayak12 <newkayak12@gmail.com>
2021-12-13 22:29:33 +0900

    iterator

--- src/00javascript/17.Symbol/2.Iterable.js
+++ src/00javascript/17.Symbol/2.Iterable.js
@@ -122,3 +122,44 @@ console.log({...obj});
 	//	arguments		arguments[Symbol.iterator]
 	//	DOM			NodeList.prorotype[Symbol.iterator]
 	//	컬렉션			  HTMLCollection.prototype[Symbol.iterator]
+
+	//for...of 문
+	/**
+	 * for...of 문은 이터러블을 순회하면서 이터러블의 요소를 변수에 할당한다. for...of문의 문법
+	 * 
+	 * 	for(변수선언문 of 이터러블) { ... }
+	 * 
+	 * for ... of 문은 for ... in 문의 형식과 매우 유사하다.
+	 * 
+	 * 	for(변수 선언문 in 객체 ) { ... }
+	 * for...in 문은 객체의 프로토타입 체인 상에 존재하는 모든 프로토타입의 프로퍼티 중에서 프로퍼티 어트리뷰트 [[Enumerable]]의 값이 true인 프로퍼티 순회하며 열거한다. 
+	 * 이떄 프로퍼티 키가 심벌인 프로퍼티 열거하지 않는다. 
+	 * 
+	 * for...of 문은 내부적으로 이터레이터의 next메소드를 호출하여 이터러블을 순회하며 next메소드가 반환한 이터레이터 리절트 객체의 value프로퍼티 값을 for...of 문의 변수에 할당한다.
+	 * 그리고 이터레이터 리절트 객체의 done프로퍼티 값이 false이면 이터러블의 순회를 계속하고 true이면 이터러블 순회를 중단한다.
+	 */ 
+
+	for(const item of [1,2,3]){
+		//item변수에 순차적으로 1,2,3이 할당된다.
+		console.log(item)
+	}
+	//위 예제의 for...of문의 내부 동작을 for문으로 표현하면 아래와 같다.
+
+	//이터러블
+	let iteratble = [1,2,3];
+
+	//이터러블의 Symbol.iterator 메소드를 호출하여 이터레이터를 생성한다.
+	iterator = iteratble[Symbol.iterator]()
+
+	for(;;){
+		//이터레이터의 next메소드를 호출하여 이터러블을 순회한다.
+		//이떄 next 메소드는 이터레이터 리절트 객체를 반환한다.
+		const res = iterator.next();
+
+		//next 메소드가 반환한 이터레이터 리절트 객체의 done프로퍼티 값이 true이면 이터러블의 순회를 중단한다.
+		if(res.done) break;
+
+		//이터레이터 리절트 객체의 value 프로퍼티의  값을 item변수에 할당한다.
+		const item = res.value;
+		console.log(item) //1,2,3
+	}
