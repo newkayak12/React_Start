61f680e3e308c8b4c010388ed481b3b4ebbe7ff3
newkayak12 <newkayak12@gmail.com>
2021-12-28 22:37:09 +0900

    node

--- src/00javascript/19.BrowserRendering/5.nodeSearch.js
+++ src/00javascript/19.BrowserRendering/5.nodeSearch.js
@@ -43,5 +43,32 @@
 
 //부모 노드 탐색
 /**
+ * 부모 노드를 탐색하려면 Node.prototype.parentNode 프로퍼티를 사용한다. 텍스트 노드는 DOM 트리의 최종단 노드인 리프 노드 이므로 부모 노드가 텍스트 노드인 경우는 없다.
+ */
+
+//형제 노드 탐색
+/**
+ * 부모 노드가 같은 형제 노드를 탐색하려면 다음과 같은 노드 탐색 프로퍼티를 사용한다. 단, 어트리뷰트 노드는 요소 노드와 연결되어 있지만 부모 노드가 같은 형제 노드가 아니기 때문에 반환되지 않는다.
  * 
+ *  Node.prototype.previousSibling : 부모 노드가 같은 형제 노드 중에서 자신의 이전 형제 노드를 탐색하여 반환한다. previousSibling 프로퍼티가 반환하는 형제 노드는 요소 노드뿐만 아니라 텍스트 노드일 수도 있다. 
+ *  
+ *  Node.prototype.nextSibling : 부모 노드가 같은 형제 노드 중에서 자신의 다음 형제 노드를 탐색하여 반환한다. nextSibling 프로퍼티가 반환하는 형제 노드는 요소 노드 뿐만 아니라 텍스크 노드일 수도 있다. 
+ * 
+ *  Element.prototype.previousElementSibling : 부모 노드가 같은 형제 요소 노드 중에서 자신의 이전 형제 요소 노드를 탐색하여 반환한다. previousElementSibling 프로퍼티는 요소 노드만 반환한다.
+ * 
+ *  Element.prototype.nextElementSibling : 부모 노드가 같은 형제 요소 노드 중에서 자신의 다음 형제 요소 노드를 탐색하여 반환한다. nextElementSibling 프로퍼티는 요소 노드만 반환한다. 
+ */
+
+// 노드 정보 취득
+/**
+ * Node.prototype.nodeType : 노드 객체의 종류, 즉 노드 타입을 나타내는 상수를 반환한다. 노드 타입 상수는 Node에 정의되어 있다. 
+ *                      - Node.ELEMENT_NODE : 요소 노드 타입을 나타내는 상수 1을 반환
+ *                      - Node.TEXT_NODE : 텍스트 노드 타입을 나타내느 상수 3을 반환
+ *                      - Node.DOCUMENT_NODE : 문서 노드 타입을 나타내는 상수 9를 반환
+ * 
+ * 
+ * Node.prototype.nodeName : 노드 이름을 문자열로 반환한다. 
+ *                      - 요소 노드 : 대문자 문자열로 태그 이름('UL', 'LI' 등)을 반환
+ *                      - 텍스트 노드 : 문자열 '#text'를 반환
+ *                      - 문서 노드 : 문자열 '#document'를 반환
  */
