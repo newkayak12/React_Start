f6f7410d54d63d3da67a700ab98b2abc89f57353
newkayak12 <newkayak12@gmail.com>
2022-01-02 20:58:22 +0900

    event

--- src/00javascript/19.BrowserRendering/7.event.js
+++ src/00javascript/19.BrowserRendering/7.event.js
@@ -133,5 +133,67 @@
  * 
  * 
  * >> 이벤트 핸들러 프로퍼티 방식
+ * window객체와 Document, HTMLElement 타입의 DOM 노드 객체는 이벤트에 대응하는 이벤트 핸들러 프로퍼티를 가지고 있다. 
+ * 이벤트 핸들러 프로퍼티의  키는 이벤트 핸들러 어트리뷰트와 아마찬가지로 onclick 과 같이 on 접두사와 이벤트의 종류를 나타내는 이벤트타입으로 이뤄져 있다. 
+ * 이벤트 핸들러 프로퍼티에 함수를 바인딩하면 에빈트 핸들러가 등록된다. 
+ * 이벤트 핸들러를 등록하기 위해서는 이벤트를 발생시킬 객체인 이벤트 타깃과 이벤트 종류를 나타내는 문자열인 이벤트 타입 그리고 이벤트 핸들러를 지정할 필요가 있다.
+ * 예를 들어, 버튼 요소가 클릭되면 handleClick 함수를 호출하도록 이벤트 핸들러를 등록하느 ㄴ경우 이벤트 타깃은 버튼 요소이고 이벤트 타입은 'click'이며 이벤트 핸들러는 handleClick 함수이다. 
  * 
+ * 
+ *  $button(이벤트 타깃).onclick(on+이벤트 타입) = function (이벤트 핸들러) () {
+ *          console.log('btnClick')
+ *  }
+ * 
+ * 이벤트 핸들러는 대부분 이벤트를 발생시킬 이벤트 타깃에 바인딩한다. 하지만 반드시 이벤트 타기셍 이벤트 핸들러를 바인딩해야하는 것은 아니다. 
+ * 이벤트 핸들러는 이벤트 타깃 또는 전파된 이벤트를 캐치할 DOM 노드 객체에 바인딩한다. 
+ * 
+ * 앞서 살펴본 '이벤트 핸드럴 어트리뷰트 방식'도 결국 DOM 노드 객체의 이벤트 핸들러 프로퍼티로 변환 되므로 결과적으로 이벤트 핸들러 프로퍼티 방식과 동일하다고 할 수 있다. 
+ * '이벤트 핸들러 프로퍼티 방식'은 '이벤트 핸들러 어트리뷰트 방식'의 HTML과 자바스크립트가 뒤섞이는 문제를 해결할 수 있다. 
+ * 하지만 이벤트 핸들러 프로퍼티에 하나의 이벤트 핸들러만 바인딩할 수 있다는 단점이 있다. 
+ * 
+ * 
+ * >>addEventListener 메소드 방식
+ * DOM Level 2에서 도입된 EventTarget.prototype.addEventListener 메소드를 사용하여 이벤트 핸들러르 ㄹ등록할 수 있다. 앞서 살펴본 '이벤트 핸들러 어트리뷰트 방식'과 '이벤트 핸들러 프로퍼티 방식'은 DOM Level 0부터 제공되던 방식이다.
+ * 
+ *  EventTarget(이벤트 타깃).addEventListener(이벤트 타입, 이벤트 핸들러, capture 사용 여부)
+ * 
+ * > capture 사용 여부 
+ * - true : capturing
+ * = false : bullbing (기본값)
+ * 
+ * 
+ * addEventListne 메소드의 첫 번째 매개변수에는 이벤트 종류를 나타내는 문자열인 이벤트 타입을 전달한다. 이때 이벤트 핸들러 프로퍼티 방식과는 달리 on 접두사를 붙이지 않는다. 두 번째 매개변수에는 이벤트 핸들러를 전달한다. 마지막 매개변수에는 이벤트를 캐치할 이벤트 전파 단계(캘처링 또는 버블링)를 지정한다. 생략하거나 false를 지정하면 버블링 단계에서 이벤트를 캐치하고,  true를 지정하면  캡처링 단계에서 이벤트를 캐치한다. 
+ */
+//<button> click me </button>
+/**
+ * const $btn = document.querySelector('button')
+ * 
+ * //이벤트 핸들러 프로퍼티 방식
+ * $btn.onclick = function(){
+ *  console.log('btnclick)
+ * }
+ * 
+ * //addEventListener 메소드 방식
+ * $btn.addEventListener('click', function(){
+ *          console.log('btnClick')
+ * })
+ * 
+ * 이벤트 핸들러 프로퍼티 방식은 이벤트 핸들러 프로퍼티에 이벤트 핸들러를 바인딩하지만 addEventListener 메소드에는 이벤트 핸들러를 인수로 전달한다.
+ * 만약 동일한 HTML 요소에서 발생한 동일한 이벤트에 대해 이벤트 핸들러 프로퍼티 방식과 addEventListener 메소드 방식을 모두 사용하여 이벤트 핸들러를 등록하면 어떻게 될까
+ */ 
+
+//<button> click! </button>
+
+/** 
+  * const $btn = document.querySelector('button')
+ * 
+ * //이벤트 핸들러 프로퍼티 방식
+ * $btn.onclick = function(){
+ *  console.log('btnclick)
+ * }
+ * 
+ * //addEventListener 메소드 방식
+ * $btn.addEventListener('click', function(){
+ *          console.log('btnClick')
+ * })
  */
