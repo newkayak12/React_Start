0c5ff501a5b3e1dd2b6e8203578da24cea4cf479
newkayak12 <newkayak12@gmail.com>
2022-08-17 17:32:51 +0900

    react

--- zerochoFree/4.ReactionRateCheck/ReactionRateCheckHooks.jsx
+++ zerochoFree/4.ReactionRateCheck/ReactionRateCheckHooks.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import {useState,useRef} from "react";
 
+const STATE = {
+    WAIT:'waiting',
+    READY:'ready',
+    NOW:'now'
+}
 const ReactionRateCheckHooks = () =>{
+    const [state, setState] = useState(STATE.WAIT)
+    const [msg, setMsg] = useState('클릭해서 시작하기')
+    const [result, setResult] = useState([])
 
+    let timeout;
+    let startTime;
+    let endTime;
+
+    const clickScreen = (e) =>{
+
+    }
     return (
         <>
+            <h2>반응속도 체크</h2>
+            <div id="screen" className={state} onClick={clickScreen}>
+                <p>{msg}</p>
+            </div>
+            {result.length === 0? null:<div>평균: {result.reduce((p, n) => p + n, 0) / result.length}ms</div>}
         </>
     )
 }
--- zerochoFree/4.ReactionRateCheck/client.jsx
+++ zerochoFree/4.ReactionRateCheck/client.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDom from 'react-dom'
 
 //class
-import ReactionRateCheck from "@/ReactionRateCheckClass";
+// import ReactionRateCheck from "@/ReactionRateCheckClass";
 
 
 //()=>{}
-// import ReactionRateCheck from "@/ReactionRateCheckHooks";
+import ReactionRateCheck from "@/ReactionRateCheckHooks";
 ReactDom.render(<ReactionRateCheck/>, document.querySelector('#root') )
